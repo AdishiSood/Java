fdfee0c3bba915e812f59d63187d46c0d2eda95f
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-02-08 22:35:17 +0530

    Create Odd_Even.java

---
+++ Arrays/Odd_Even.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Odd_Even
+{
+    public static void main(String[] args) 
+    {
+        int n;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter no. of elements you want in array:");
+        n = s.nextInt();
+        int a[] = new int[n];
+        System.out.println("Enter all the elements:");
+        for (int i = 0; i < n; i++) 
+        {
+            a[i] = s.nextInt();
+        }
+        System.out.print("Odd numbers:");
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(a[i] % 2 != 0)
+            {
+                System.out.print(a[i]+" ");
+            }
+        }
+        System.out.println("");
+        System.out.print("Even numbers:");
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(a[i] % 2 == 0)
+            {
+                System.out.print(a[i]+" ");
+            }
+        }
+    }
+}
