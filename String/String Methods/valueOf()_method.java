ec473a3e8e2cad42b909e09b4bd303514a8fc022
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-02-09 20:07:15 +0530

    Create valueOf()_method.java

---
+++ String/String Methods/valueOf()_method.java
@@ -0,0 +1,17 @@
+/*
+
+The java string valueOf() method converts different types of values into string. 
+By the help of string valueOf() method, you can convert int to string, long to string, 
+boolean to string, character to string, float to string, double to string, object to string and char array to string.
+
+*/
+
+public class valueOf
+{  
+public static void main(String args[])
+{  
+int value=30;  
+String s1=String.valueOf(value);  
+System.out.println(s1+10);  //concatenating string with 10  
+}
+}  
