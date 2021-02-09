5307095578604b0f66fc65163dc7c9b5d6a4ae25
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-02-09 20:11:00 +0530

    Create toCharArray()_method.java

---
+++ String/String Methods/toCharArray()_method.java
@@ -0,0 +1,19 @@
+/*
+
+The java string toCharArray() method converts this string into character array. 
+It returns a newly created character array, its length is similar to this string and its contents are initialized with the characters of this string.
+
+*/
+
+public class toCharArray
+{  
+public static void main(String args[])
+{  
+String s1="hello";  
+char[] ch=s1.toCharArray();  
+for(int i=0;i<ch.length;i++)
+{  
+System.out.print(ch[i]);  
+}  
+}
+}  
