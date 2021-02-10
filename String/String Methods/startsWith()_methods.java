fa0dcdbec0cb22c0861dfcbee16b948a07e30f83
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-02-11 01:17:07 +0530

    Create startsWith()_methods.java

---
+++ String/String Methods/startsWith()_methods.java
@@ -0,0 +1,16 @@
+/*
+
+The java string startsWith() method checks if this string starts with given prefix.
+It returns true if this string starts with given prefix else returns false.
+
+*/
+
+public class StartsWith
+{  
+public static void main(String args[])
+{  
+String s1="java string split method by github";  
+System.out.println(s1.startsWith("ja"));  //true
+System.out.println(s1.startsWith("java string"));  //true
+}
+}  
