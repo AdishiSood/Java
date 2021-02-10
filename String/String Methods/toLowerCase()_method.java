e15259a67d444b0115794a1dd9936d7bc15e2c63
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-02-10 20:29:43 +0530

    Update toLowerCase()_method.java

--- String/String Methods/toLowerCase()_method.java
+++ String/String Methods/toLowerCase()_method.java
@@ -13,6 +13,6 @@ public static void main(String args[])
 {  
 String s1="HELLO stRIng";  
 String s1lower=s1.toLowerCase();  
-System.out.println(s1lower);  
+System.out.println(s1lower);  //hello string
 }
 }  
