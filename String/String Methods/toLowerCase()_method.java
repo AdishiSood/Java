5f081dbfa39ba91dd234fc6422714a52d72b168c
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-02-10 20:25:12 +0530

    Create toLowerCase()_method.java

---
+++ String/String Methods/toLowerCase()_method.java
@@ -0,0 +1,18 @@
+/*
+
+The java string toLowerCase() method returns the string in lowercase letter. In other words, it converts all characters of the string into lower case letter.
+
+The toLowerCase() method works same as toLowerCase(Locale.getDefault()) method. It internally uses the default locale.
+
+*/
+
+
+public class toLowerCase
+{  
+public static void main(String args[])
+{  
+String s1="HELLO stRIng";  
+String s1lower=s1.toLowerCase();  
+System.out.println(s1lower);  
+}
+}  
