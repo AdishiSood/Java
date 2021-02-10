3b79716c1a92a069ef9608504bcbbaa865d7e39f
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-02-10 20:27:53 +0530

    Create substring().java

---
+++ String/String Methods/substring().java
@@ -0,0 +1,18 @@
+/*
+
+The java string substring() method returns a part of the string.
+
+We pass begin index and end index number position in the java substring method where start index is inclusive and end index is exclusive.
+In other words, start index starts from 0 whereas end index starts from 1.
+
+*/
+
+public class Substring
+{  
+public static void main(String args[])
+{  
+String s1="github";  
+System.out.println(s1.substring(2,4));  //returns th
+System.out.println(s1.substring(2));  //returns thub
+}
+}  
