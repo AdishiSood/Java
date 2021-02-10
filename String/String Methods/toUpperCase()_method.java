f900be666470740aa76aa2c503f9eb04f81a72ff
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-02-10 20:30:08 +0530

    Update toUpperCase()_method.java

--- String/String Methods/toUpperCase()_method.java
+++ String/String Methods/toUpperCase()_method.java
@@ -13,6 +13,6 @@ public static void main(String args[])
 {  
 String s1="hello string";  
 String s1upper=s1.toUpperCase();  
-System.out.println(s1upper);  
+System.out.println(s1upper);  //returns HELLO STRING
 }
 }  
