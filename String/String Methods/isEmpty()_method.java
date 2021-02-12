2b5dde8649a140f6a7366a04b9f5b8f6fff8a6a3
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-02-13 01:06:19 +0530

    Create isEmpty()_method.java

---
+++ String/String Methods/isEmpty()_method.java
@@ -0,0 +1,20 @@
+/*
+
+The java string isEmpty() method checks if this string is empty or not. 
+It returns true, if length of string is 0 otherwise false. In other words, true is returned if string is empty otherwise it returns false.
+
+The isEmpty() method of String class is included in java string since JDK 1.6.
+
+*/
+
+public class isEmpty
+{  
+public static void main(String args[])
+{  
+String s1="";  
+String s2="github";  
+  
+System.out.println(s1.isEmpty());  //true
+System.out.println(s2.isEmpty());  //false
+}
+}  
