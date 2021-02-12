585c5a6c3add0b4e7b88f5fbf86bbc05bf8adcc2
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-02-13 01:14:48 +0530

    Create length()_method.java

---
+++ String/String Methods/length()_method.java
@@ -0,0 +1,15 @@
+/*
+The java string length() method length of the string. It returns count of total number of characters.
+The length of java string is same as the unicode code units of the string.
+
+*/
+public class length
+{  
+public static void main(String args[])
+{  
+String s1="github";  
+String s2="java";  
+System.out.println("string length is: "+s1.length());//string length is: 6 
+System.out.println("string length is: "+s2.length());//string length is: 4  
+}
+}  
