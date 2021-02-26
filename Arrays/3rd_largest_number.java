c500ec118c2ecb5deecef3ac69a282e712e86d1d
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-02-26 23:25:05 +0530

    Create 3rd_largest_number.java

---
+++ Arrays/3rd_largest_number.java
@@ -0,0 +1,29 @@
+/*
+
+Java Program to find Third Largest Number in an Array
+
+*/
+
+public class ThirdLargestNumber{  
+public static int getThirdLargest(int[] a, int total){  
+int temp;  
+for (int i = 0; i < total; i++)   
+        {  
+            for (int j = i + 1; j < total; j++)   
+            {  
+                if (a[i] > a[j])   
+                {  
+                    temp = a[i];  
+                    a[i] = a[j];  
+                    a[j] = temp;  
+                }  
+            }  
+        }  
+       return a[total-3];  
+}  
+public static void main(String args[]){  
+int a[]={7,7,10,16,32,12};  
+int b[]={5,62,19,72,23,20,54};  
+System.out.println("Third Largest: "+getThirdLargest(a,6));  
+System.out.println("Third Largest: "+getThirdLargest(b,7));  
+}}  
