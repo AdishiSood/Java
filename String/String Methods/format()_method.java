acf748898bfdfe547771281332814c7eccb68abf
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-02-27 23:48:50 +0530

    Update and rename format().java to format()_method.java

---
+++ String/String Methods/format()_method.java
@@ -0,0 +1,9 @@
+/*
+
+The java string format() method returns the formatted string by given locale, format and arguments.
+
+If you don't specify the locale in String.format() method, it uses default locale by calling Locale.getDefault() method.
+
+The format() method of java language is like sprintf() function in c language and printf() method of java language.
+
+*/
