e9be884c7aaa564ff829f3c3c6f0f72769552285
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-02-27 23:50:23 +0530

    Update format()_method.java

--- String/String Methods/format()_method.java
+++ String/String Methods/format()_method.java
@@ -7,3 +7,18 @@ If you don't specify the locale in String.format() method, it uses default local
 The format() method of java language is like sprintf() function in c language and printf() method of java language.
 
 */
+
+public class Format{
+  
+public static void main(String args[])
+{  
+String name="raghav";  
+String sf1=String.format("name is %s",name);  
+String sf2=String.format("value is %f",56.27362);  
+String sf3=String.format("value is %32.12f",28.9387);//returns 12 char fractional part
+  
+System.out.println(sf1);  
+System.out.println(sf2);  
+System.out.println(sf3);  
+}
+}  
