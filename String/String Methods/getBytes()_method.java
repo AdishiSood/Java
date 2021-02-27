60fba90729a0d9187cfa66601e3d9bee56dae8e8
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-02-27 23:52:34 +0530

    Update getBytes()_method.java

--- String/String Methods/getBytes()_method.java
+++ String/String Methods/getBytes()_method.java
@@ -1,1 +1,10 @@
+/*
 
+The java string getBytes() method returns the byte array of the string. In other words, it returns sequence of bytes.
+
+There are 3 variant of getBytes() method. The signature or syntax of string getBytes() method is given below:
+public byte[] getBytes()  
+public byte[] getBytes(Charset charset)  
+public byte[] getBytes(String charsetName)throws UnsupportedEncodingException  
+
+*/
