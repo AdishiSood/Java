ab875d2c3b800c4b90332c3100efa28bd7ddff76
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-02-27 23:50:59 +0530

    Update format()_method.java

--- String/String Methods/format()_method.java
+++ String/String Methods/format()_method.java
@@ -17,8 +17,8 @@ String sf1=String.format("name is %s",name);
 String sf2=String.format("value is %f",56.27362);  
 String sf3=String.format("value is %32.12f",28.9387);//returns 12 char fractional part
   
-System.out.println(sf1);  
-System.out.println(sf2);  
-System.out.println(sf3);  
+System.out.println(sf1);  //name is raghav
+System.out.println(sf2);  //value is 56.273620
+System.out.println(sf3);  //value is 28.938700000000
 }
 }  
