db7dd521c3435c6083c44bfe6f7735e81a809109
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-02-28 22:18:14 +0530

    Update getBytes()_method.java

--- String/String Methods/getBytes()_method.java
+++ String/String Methods/getBytes()_method.java
@@ -8,3 +8,16 @@ public byte[] getBytes(Charset charset)
 public byte[] getBytes(String charsetName)throws UnsupportedEncodingException  
 
 */
+public class StringExample
+{  
+public static void main(String args[])
+{  
+String s1="gist";  
+char ch[]={'g','i','t','h','u','b'};  
+String s2=new String(ch);  
+String s3=new String("programming");  
+System.out.println(s1);  
+System.out.println(s2);  
+System.out.println(s3);  
+}
+}  
