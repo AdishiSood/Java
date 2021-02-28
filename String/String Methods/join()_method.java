7490968470fbf7f880fb93b5ab7f3b58d3f9e220
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-02-28 22:22:42 +0530

    Create join()_method.java

---
+++ String/String Methods/join()_method.java
@@ -0,0 +1,7 @@
+/*
+
+The java string join() method returns a string joined with given delimiter. In string join method, delimiter is copied for each elements.
+
+In case of null element, "null" is added. The join() method is included in java string since JDK 1.8.
+
+*/
