31436d31575e55a4e68813a5ed57ae7f71417987
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-02-28 22:24:48 +0530

    Update join()_method.java

--- String/String Methods/join()_method.java
+++ String/String Methods/join()_method.java
@@ -5,3 +5,11 @@ The java string join() method returns a string joined with given delimiter. In s
 In case of null element, "null" is added. The join() method is included in java string since JDK 1.8.
 
 */
+public class join
+{  
+public static void main(String args[])
+{  
+String joinString1 = String.join("_","projects","on","github");  
+System.out.println(joinString1);  //projects_on_github
+}
+}  
