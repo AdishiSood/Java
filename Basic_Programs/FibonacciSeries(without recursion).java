14c5a04c542a7f27c07945e983afa480e189f757
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-03-01 20:10:03 +0530

    Create FibonacciSeries(without recursion).java

---
+++ Basic_Programs/FibonacciSeries(without recursion).java
@@ -0,0 +1,27 @@
+/*
+In fibonacci series, next number is the sum of previous two numbers for example 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 etc. The first two numbers of fibonacci series are 0 and 1.
+
+There are two ways to write the fibonacci series program in java:
+
+Fibonacci Series without using recursion
+Fibonacci Series using recursion
+
+*/
+
+class FibonacciSeries
+{  
+public static void main(String args[])  
+{    
+ int n1=0,n2=1,n3,i,count=10;    
+ System.out.print(n1+" "+n2);//printing 0 and 1    
+    
+ for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
+ {    
+  n3=n1+n2;    
+  System.out.print(" " +n3);    
+  n1=n2;    
+  n2=n3;    
+ }    
+  
+}
+}  
