f412aaacd3cbc09467339579f3b9f5d1aeb19dd6
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-03-02 19:20:55 +0530

    Update intern()method.java

--- String/String Methods/intern()method.java
+++ String/String Methods/intern()method.java
@@ -1,1 +1,7 @@
+/*
+The java string intern() method returns the interned string. It returns the canonical representation of string.
+
+It can be used to return string from memory, if it is created by new keyword. It creates exact copy of heap string object in string constant pool.
+
+*/
 
