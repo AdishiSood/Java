fe9395d7c0e10b34779f44ad3462d99025e80b72
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-03-15 11:46:16 +0530

    Update 2nd_largest_number.java

--- Arrays/2nd_largest_number.java
+++ Arrays/2nd_largest_number.java
@@ -1,1 +1,27 @@
 
+public class SecondLargestInArray{  
+public static int getSecondLargest(int[] a, int total)
+{  
+int temp;  
+for (int i = 0; i < total; i++)   
+        {  
+            for (int j = i + 1; j < total; j++)   
+            {  
+                if (a[i] > a[j])   
+                {  
+                    temp = a[i];  
+                    a[i] = a[j];  
+                    a[j] = temp;  
+                }  
+            }  
+        }  
+       return a[total-2];  
+}  
+public static void main(String args[])
+{  
+int a[]={41,22,25,2,32,12};  
+int b[]={44,6,99,7,33,2,55};  
+System.out.println("Second Largest: "+getSecondLargest(a,6));  
+System.out.println("Second Largest: "+getSecondLargest(b,7));  
+}
+}  
