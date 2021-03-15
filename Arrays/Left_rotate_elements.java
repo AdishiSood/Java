be7147d0cea2ab253ccb3696a0b5ba18060d4e94
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-03-15 11:42:52 +0530

    Update Left_rotate_elements.java

--- Arrays/Left_rotate_elements.java
+++ Arrays/Left_rotate_elements.java
@@ -1,1 +1,35 @@
-
+/*
+In this program, we need to rotate the elements of an array towards the left by the specified number of times. 
+In the left rotation, each element of the array will be shifted to its left by one position and the first element of the array will be added to end of the list.
+*/
+class LeftRotate{  
+    public static void main(String[] args) {  
+        //Initialize array  
+        int [] arr = new int [] {1, 2, 3, 4, 5};  
+        //n determine the number of times an array should be rotated  
+        int n = 3;  
+        //Displays original array  
+        System.out.println("Original array: ");  
+        for (int i = 0; i < arr.length; i++) {  
+            System.out.print(arr[i] + " ");  
+        }  
+        //Rotate the given array by n times toward left  
+        for(int i = 0; i < n; i++){  
+            int j, first;  
+            //Stores the first element of the array  
+            first = arr[0];  
+            for(j = 0; j < arr.length-1; j++){  
+                //Shift element of array by one  
+                arr[j] = arr[j+1];  
+            }  
+            //First element of array will be added to the end  
+            arr[j] = first;  
+        }  
+        System.out.println();  
+        //Displays resulting array after rotation  
+        System.out.println("Array after left rotation: ");  
+        for(int i = 0; i< arr.length; i++){  
+            System.out.print(arr[i] + " ");  
+        }  
+    }  
+}  
