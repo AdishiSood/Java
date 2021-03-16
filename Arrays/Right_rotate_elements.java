758110c5c860c44195013da955abfbe9af1435bb
Adishi <adishi1508.cse18@chitkara.edu.in>
2021-03-16 12:18:17 +0530

    Update Right_rotate_elements.java

--- Arrays/Right_rotate_elements.java
+++ Arrays/Right_rotate_elements.java
@@ -1,1 +1,9 @@
 
+/*
+In this program, we need to rotate the elements of array towards its right by the specified number of times.
+An array is said to be right rotated if all elements of the array are moved to its right by one position. 
+One approach is to loop through the array by shifting each element of the array to its next position.
+The last element of the array will become the first element of the rotated array.
+*/
+class RotateRight {    
+   
